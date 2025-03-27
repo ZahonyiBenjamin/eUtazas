f5edcf38f7aeaeb096d71410710fcfa1d181d47c
ZahonyiBenjamin <150019009+ZahonyiBenjamin@users.noreply.github.com>
2025-03-27 10:45:31 +0100

    Add files via upload

---
+++ eUtazas/eUtazas/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUtazas
+{
+    class Program
+    {
+        static List<UtasAdat> utas_adatok = new List<UtasAdat>();
+        static void Main(string[] args)
+        {
+            AdatokLetarolasa();
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+
+            Console.ReadKey();
+        }
+
+        private static void Feladat5()
+        {
+            var ervenyes_utasok = utas_adatok.Where(x => x.ervenyessegi_ido.Length == 8 && (x.ervenyessegi_ev > x.felszallas_ev || x.ervenyessegi_ev >= x.felszallas_ev && x.ervenyessegi_honap > x.felszallas_honap || x.ervenyessegi_ev >= x.felszallas_ev && x.ervenyessegi_honap >= x.felszallas_honap && x.ervenyessegi_nap >= x.felszallas_nap)).ToList();
+            int ingyenes_db = ervenyes_utasok.Where(x => x.jegy_tipus == "NYP" || x.jegy_tipus == "RVS" || x.jegy_tipus == "GYK").Count();
+            int kedvezmenyes_db = ervenyes_utasok.Where(x => x.jegy_tipus == "TAB" || x.jegy_tipus == "NYB").Count();
+
+            Console.WriteLine($"5. feladat" +
+                $"\nIngyenesen utazók száma: {ingyenes_db} fő" +
+                $"\nA kedvezményesen utazók száma: {kedvezmenyes_db} fő");
+        }
+
+        static void Feladat4()
+        {
+            var legtobb_felszallas = utas_adatok.GroupBy(x => x.sorszam).OrderByDescending(x => x.Count()).ThenBy(x => x.Key).FirstOrDefault();
+
+            Console.WriteLine($"4. feladat" +
+                $"\nA legtöbb utas ({legtobb_felszallas.Count()} fő) a {legtobb_felszallas.Key}. megállóban proóbált felszállni.");
+        }
+
+        static void Feladat3()
+        {
+            int elutasitott_utasok = utas_adatok.Where(x => x.ervenyessegi_ido == "0" || (x.ervenyessegi_ido.Length == 8 && (x.ervenyessegi_ev < x.felszallas_ev || x.ervenyessegi_ev <= x.felszallas_ev && x.ervenyessegi_honap < x.felszallas_honap || x.ervenyessegi_ev <= x.felszallas_ev && x.ervenyessegi_honap <= x.felszallas_honap && x.ervenyessegi_nap < x.felszallas_nap))).Count();
+
+            Console.WriteLine($"3. feladat" +
+                $"\nA buszra {elutasitott_utasok} utas nem szállhatott fel");
+        }
+
+        static void Feladat2()
+        {
+            Console.WriteLine($"2. feladat" +
+                $"\nA buszra {utas_adatok.Count} utas akart felszállni.");
+        }
+
+        static void AdatokLetarolasa()
+        {
+            string[] f = File.ReadAllLines("utasadat.txt");
+
+            foreach (var line in f)
+            {
+                utas_adatok.Add(new UtasAdat(line));
+            }
+
+            //Console.WriteLine($"{utas_adatok[0].sorszam} {utas_adatok[0].felszallas_ev}.{utas_adatok[0].felszallas_honap}.{utas_adatok[0].felszallas_nap} {utas_adatok[0].felszallas_ora}:{utas_adatok[0].felszallas_perc} {utas_adatok[0].jegy_tipus} {utas_adatok[0].ervenyessegi_ev}.{utas_adatok[0].ervenyessegi_honap}.{utas_adatok[0].ervenyessegi_nap}");
+        }
+    }
+}
---
+++ eUtazas/eUtazas/UtasAdat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUtazas
+{
+    class UtasAdat
+    {
+        public int sorszam {  get; set; }
+        public string felszallas_datum {  get; set; }
+        public int felszallas_ev { get; set; }
+        public int felszallas_honap { get; set; }
+        public int felszallas_nap { get; set; }
+        public int felszallas_ora { get; set; }
+        public int felszallas_perc { get; set; }
+        public int kartya_azonosito {  get; set; }
+        public string jegy_tipus {  get; set; }
+        public string ervenyessegi_ido {  get; set; }
+        public int ervenyessegi_ev { get; set; }
+        public int ervenyessegi_honap { get; set; }
+        public int ervenyessegi_nap { get; set; }
+
+        public UtasAdat(string sor)
+        {
+            string[] sz = sor.Split(' ');
+
+            sorszam = int.Parse(sz[0]);
+
+            felszallas_datum = sz[1];
+            felszallas_ev = int.Parse(felszallas_datum.Substring(0, 4));
+            felszallas_honap = int.Parse(felszallas_datum.Substring(4, 2));
+            felszallas_nap = int.Parse(felszallas_datum.Substring(6, 2));
+            felszallas_ora = int.Parse(felszallas_datum.Substring(9, 2));
+            felszallas_perc = int.Parse(felszallas_datum.Substring(11, 2));
+
+            kartya_azonosito = int.Parse(sz[2]);
+            jegy_tipus = sz[3];
+
+            ervenyessegi_ido = sz[4];
+            if (ervenyessegi_ido.Length == 8)
+            {
+                ervenyessegi_ev = int.Parse(ervenyessegi_ido.Substring(0, 4));
+                ervenyessegi_honap = int.Parse(ervenyessegi_ido.Substring(4, 2));
+                ervenyessegi_nap = int.Parse(ervenyessegi_ido.Substring(6, 2));
+            }
+            else
+            {
+                ervenyessegi_ev = 0;
+                ervenyessegi_honap = 0;
+                ervenyessegi_nap = 0;
+            }
+        }
+    }
+}
